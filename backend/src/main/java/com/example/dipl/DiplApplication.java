ac3c4b79ea6d730ba3fb4ef15c8f5275d3286790
Hans <Tanzer.Hans@student.htlwrn.ac.at>
2025-12-03 19:01:45 +0100

    Test

---
+++ backend/src/main/java/com/example/dipl/DiplApplication.java
@@ -0,0 +1,13 @@
+package com.example.dipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/dipl/DiplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dipl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
